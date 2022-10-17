a151d100ec50d9fcccce0a7a4299364fb514e9d2
Ruslan Khazhin <rkhazhin@fix.ru>
2022-10-17 10:52:16 +0300

    main: remname classes, add intervals transform

--- src/DateTransformer.php
+++ src/DateTransformer.php
@@ -2,6 +2,8 @@
 
 namespace Anastasya\DateTransformers;
 
+use DateTime;
+use Exception;
 use Ruslanovich\DateConverter\Enums\RowTypeEnum;
 use Ruslanovich\DateConverter\Exceptions\ConvertToDateCollectionException;
 use Ruslanovich\DateConverter\Exceptions\ConvertToDateException;
@@ -13,8 +15,51 @@ use Ruslanovich\DateConverter\Models\DateModel;
 use Ruslanovich\DateConverter\Services\RowToModelConverter;
 use Ruslanovich\DateConverter\Storages\PatternStorages\ArrayPatternStorage;
 
-class ToDateIntervalsTransformer
+class DateTransformer
 {
+    /**
+     * @param string $date
+     * @param string|null $outputDateFormat
+     * @return string
+     * @throws Exception
+     */
+    public static function transformToString(string $date, ?string $outputDateFormat = 'd.m.Y'): string
+    {
+        $rawDate = preg_replace('/\s{2,}/', ' ', trim($date));
+        /**
+         * Конвертируем даты типа: "22 Sep. 1978", "1987-12-15", "18.07.1990", "15 February 1983"
+         */
+        if (DateTime::createFromFormat('Y-m-d', $rawDate) ||
+            DateTime::createFromFormat('d.m.Y', $rawDate) ||
+            DateTime::createFromFormat('d F Y', $rawDate) ||
+            DateTime::createFromFormat('d M. Y', $rawDate)) {
+            return (new DateTime($rawDate))->format($outputDateFormat);
+        }
+
+        /**
+         * Конвертируем дату типа: "2 января 1964 г"
+         */
+        $months = ["января", "февраля", "марта", "апреля", "мая", "июня", "июля",
+            "августа", "сентября", "октября", "ноября", "декабря"];
+        $monthRegExpression = implode("|", $months);
+
+        $rawWithoutSpaces = str_replace(' ', '', $rawDate);
+        $pattern = '/^([0-9]{1,2})(' . $monthRegExpression . ')([0-9]{4})(г)?(.)?$/ui';
+        if (preg_match_all($pattern, $rawWithoutSpaces, $matches)) {
+            $month = $matches[2][0];
+            foreach ($months as $number => $value) {
+                if (mb_strtolower($month) === $value) {
+                    $monthNumber = $number + 1;
+                }
+            }
+
+            $formattedDate = preg_replace($pattern, '$1.' . $monthNumber . '.$3', $rawWithoutSpaces);
+            return (new DateTime($formattedDate))->format($outputDateFormat);
+        }
+
+        return $rawDate;
+    }
+
     /**
      * @param string $inputDate
      * @return array
@@ -25,7 +70,7 @@ class ToDateIntervalsTransformer
      * @throws InvalidIntervalException
      * @throws InvalidPatternResourceException
      */
-    public static function transform(string $inputDate): array
+    public static function transformToIntervals(string $inputDate): array
     {
         $config = require __DIR__ . '/../config/date-patterns.php';
         $patternStorage = new ArrayPatternStorage($config);
@@ -92,10 +137,11 @@ class ToDateIntervalsTransformer
      * @param int $number
      * @return string
      */
-    private static function formatNumber(int $number): string {
-        $stringNumbers = ["", "01","02","03","04","05","06","07","08","09"];
+    private static function formatNumber(int $number): string
+    {
+        $stringNumbers = ["", "01", "02", "03", "04", "05", "06", "07", "08", "09"];
 
-        if(0 < $number && $number < 10 ){
+        if (0 < $number && $number < 10) {
             return $stringNumbers[$number];
         }
 
--- test/to-stringt-test.php
+++ test/to-stringt-test.php
@@ -1,14 +1,14 @@
 <?php
 
-use Anastasya\DateTransformers\ToStringDateTransformer;
+use Anastasya\DateTransformers\DateTransformer;
 
 require './vendor/autoload.php';
 
-echo ToStringDateTransformer::transform('22 Sep. 1978') . "\n";
-echo ToStringDateTransformer::transform('1987-12-15') . "\n";
-echo ToStringDateTransformer::transform('18.07.1990') . "\n";
-echo ToStringDateTransformer::transform('15 February 1983') . "\n";
-echo ToStringDateTransformer::transform('2 января 1964 г') . "\n";
-echo ToStringDateTransformer::transform('2 Января 1964 Г') . "\n";
-echo ToStringDateTransformer::transform('2 Января 1964 Г - 2 июля 1964 Г') . "\n";
-echo ToStringDateTransformer::transform('2 Января 1964 Г.') . "\n";
+echo DateTransformer::transformToString('22 Sep. 1978') . "\n";
+echo DateTransformer::transformToString('1987-12-15') . "\n";
+echo DateTransformer::transformToString('18.07.1990') . "\n";
+echo DateTransformer::transformToString('15 February 1983') . "\n";
+echo DateTransformer::transformToString('2 января 1964 г') . "\n";
+echo DateTransformer::transformToString('2 Января 1964 Г') . "\n";
+echo DateTransformer::transformToString('2 Января 1964 Г - 2 июля 1964 Г') . "\n";
+echo DateTransformer::transformToString('2 Января 1964 Г.') . "\n";
